3833ad7366efb02f2fe7a706b9e77e5e5e47d7ce
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-12-28 17:01:59 +0100

    add preliminary code for Eval class

--- src/Eval.Csharp/Eval.cs
+++ src/Eval.Csharp/Eval.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Eval.Csharp
 {
     public class Eval
     {
-        public Eval()
+        private string _code;
+        private Dictionary<string, object> _variables;
+
+        public Eval(string code)
         {
-            
+            this._code = code;
+            this._variables = new Dictionary<string, object>();
+        }
+
+        public Eval AddVariable<T>(string identifier, T @object)
+        {
+            this._variables.Add(identifier, @object);
+            return this;
+        }
+
+        public Func<object> Evaluate()
+        {
+            return () => new Object();
         }
     }
 }
