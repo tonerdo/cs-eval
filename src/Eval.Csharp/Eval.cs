ff4f28cf4e2880337558f56a8658120d9a02f368
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-12-29 23:23:03 +0100

    add evaluate method for evaluation C# expressions

--- src/Eval.Csharp/Eval.cs
+++ src/Eval.Csharp/Eval.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Eval.Csharp
 {
@@ -20,9 +26,40 @@ namespace Eval.Csharp
             return this;
         }
 
-        public Func<object> Evaluate()
+        private SyntaxTree ValidateCode()
         {
-            return () => new Object();
+            if (string.IsNullOrEmpty(_code) || string.IsNullOrWhiteSpace(_code))
+                throw new Exception("Code cannot be empty or null");
+
+            var tree = CSharpSyntaxTree.ParseText(_code, CSharpParseOptions.Default.WithKind(SourceCodeKind.Script));
+            var diagnostics = tree.GetDiagnostics().ToList();
+
+            if (diagnostics.Count > 0)
+                throw new Exception(diagnostics[0].ToString());
+
+            return tree;
         }
+
+        private ExpressionTransformer GetExpressionTransformer()
+        {
+            ExpressionStatementSyntax expr = ValidateCode().GetRoot()
+                .DescendantNodes()
+                .OfType<ExpressionStatementSyntax>()
+                .First();
+
+            ExpressionTransformer transformer = new ExpressionTransformer(expr, _variables);
+            return transformer;
+        }
+
+        private T EvaluateImpl<T>()
+        {
+            var transformer = GetExpressionTransformer();
+            var expression = Expression.Lambda<T>(transformer.Transform());
+            return expression.Compile();
+        }
+
+        public Func<T> Evaluate<T>() => EvaluateImpl<Func<T>>();
+
+        public Action Evaluate() => EvaluateImpl<Action>();
     }
 }
